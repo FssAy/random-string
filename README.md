38fa698e510f69be96a58f44879b7ca6dc43f913
Dmitrij <49795742+DmitrijVC@users.noreply.github.com>
2020-10-10 12:04:50 +0200

    v0.1.0

---
+++ README.md
@@ -0,0 +1,55 @@
+# random-string
+A simple crate that allows you to generate random strings based on a given charset and length.
+
+## Description
+This is just a test crate for my personal use, and I don't think that there would be more updates. <br>
+If you would like to change or add something, or even create a better version feel free to do so. 
+
+## Usage
+Add a dependence in `Cargo.toml`
+```toml
+[dependencies]
+random-string = "0.1.0"
+``` 
+
+## Functions
+
+`RandomString::get_charset(Charset)` <br> 
+Get a ready charset by passing an enum `Charset`. <br>
+The output is a String containing certain characters that will be used in the generation.<br>
+<br>
+`RandomString::generate(i32, String)` - Generate a random string with a certain length as `i32` and based on a `string` charset. 
+The output is a Result with the generated String if succeeded, or an error message (static str) if failed. 
+
+*Every charset is a String containing characters that you would like to use in generation process.
+Remember that it can't be empty.*
+
+## Enums
+Use this enum in `RandomString::get_charset` function to get a charset.
+```rust
+enum Charset {
+    ASCII,              // ASCII characters
+    Letters,            // All lowercase and uppercase letters
+    LettersLowercase,   // Lowercase letters from a to z
+    LettersUppercase,   // Uppercase letters from A to Z
+    Numbers,            // Numbers from 0 to 9
+    Special             // All the special characters I could find on my keyboard
+}
+```
+
+## Examples
+```rust
+use random_string::{RandomString, Charset};
+
+fn main() {
+    
+    let charset = RandomString::get_charset(Charset::Numbers);
+    // or
+    let charset = "1234567890".to_string();
+    
+    match RandomString::generate(6, charset) {
+        Ok(string) => println!("{}", string),
+        Err(msg) => println!("Error: {}", msg)
+    }
+}
+```
---
+++ src/lib.rs
@@ -0,0 +1,62 @@
+use random_number::random;
+use std::cmp::min;
+
+pub enum Charset {
+    ASCII,
+    Letters,
+    LettersLowercase,
+    LettersUppercase,
+    Numbers,
+    Special,
+}
+
+struct RandomString;
+
+impl RandomString {
+
+    fn split_by_length(data: String, length: usize) -> Result<Vec<String>, &'static str> {
+        if data == "".to_string() {
+            return Err("String data can't be empty")
+        }
+
+        let mut vector_result: Vec<String> = vec![];
+        let mut str_temp: &str = data.as_str();
+
+        while !str_temp.is_empty() {
+            let (pref, suf) = str_temp.split_at(min(length, str_temp.len()));
+            vector_result.push(pref.to_string());
+            str_temp = suf;
+        }
+
+        Ok(vector_result)
+    }
+
+    pub fn get_charset(charset: Charset) -> String {
+        return match charset {
+            Charset::ASCII => "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%^&*()_+-={}[]:\";'\\|,.<>/?`~".to_string(),
+            Charset::Letters => "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM".to_string(),
+            Charset::LettersLowercase => "qwertyuiopasdfghjklzxcvbnm".to_string(),
+            Charset::LettersUppercase => "QWERTYUIOPASDFGHJKLZXCVBNM".to_string(),
+            Charset::Numbers => "1234567890".to_string(),
+            Charset::Special => "!@#$%^&*()_+-={}[]:\";'\\|,.<>/?`~".to_string(),
+        }
+    }
+
+    pub fn generate(length: i32, charset: String) -> Result<String, &'static str>{
+        let vec_charset;
+        match RandomString::split_by_length(charset, 1) {
+            Ok(result) => vec_charset = result,
+            Err(msg) => return Err(msg)
+        }
+
+        let mut random_string: String = "".to_string();
+        let mut i: u8;
+        for _ in 0..length {
+            i = random!(..=(vec_charset.len() - 1) as u8);
+            random_string += vec_charset[i as usize].as_str();
+        }
+
+        Ok(random_string)
+    }
+
+}
