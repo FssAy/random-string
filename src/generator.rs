ebf89a439991a1ce17c3cdb1f68041bf81b13b51
Skarlett <360d@pm.me>
2021-07-03 03:51:55 -0500

    Removed redundant code, and formalized the libraries entry function with types provided by the standard library

--- src/generator.rs
+++ src/generator.rs
@@ -1,5 +1,3 @@
-use std::collections::VecDeque;
-use std::fmt::{Display, Formatter, Debug, Result};
 use crate::*;
 
 
@@ -14,74 +12,17 @@ use crate::*;
 ///
 /// ```
 /// use random_string::generate;
-/// use random_string::Charset;
 ///
-/// let charset = Charset::new("123");
 /// println!("{}", generate(6, &charset));
 /// ```
-pub fn generate(length: usize, charset: &Charset) -> GenResult {
-    let mut result = VecDeque::<char>::new();
-    let body = charset.borrow_body();
 
+pub fn generate(length: usize, charset: &str) -> String {
+    let body = charset;
+    let result = String::with_capacity(length);
+    
     for _ in 0..length {
-        result.push_back(
-            *body.get(fastrand::usize(0..charset.length())).unwrap()
-        )
+      body.chars().nth(fastrand::usize(0..charset.len()));
     }
 
-    GenResult {
-        body: result
-    }
-}
-
-
-/// Generation result holding a vector of chars
-pub struct GenResult {
-    body: VecDeque<char>,
-}
-
-impl GenResult {
-    pub fn to_string(&self) -> String {
-        let mut result = String::new();
-        for ch in self.body.iter() {
-            result.push(*ch)
-        }
-        result
-    }
-
-    pub fn len(&self) -> usize {
-        self.body.len()
-    }
-
-    pub fn borrow_body(&self) -> &VecDeque<char> {
-        &self.body
-    }
-
-    pub fn borrow_body_mut(&mut self) -> &mut VecDeque<char> {
-        &mut self.body
-    }
-
-    pub fn clone_body(&self) -> VecDeque<char> {
-        self.body.clone()
-    }
-}
-
-impl Display for GenResult {
-    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-        write!(
-            f,
-            "{}",
-            self.to_string(),
-        )
-    }
-}
-
-impl Debug for GenResult {
-    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-        write!(
-            f,
-            "{:?}",
-            self.borrow_body(),
-        )
-    }
+    result
 }
