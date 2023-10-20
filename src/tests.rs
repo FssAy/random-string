a6a48db43d7b82fbb60748aaf37b2382bdc8ad30
Dmitrij <49795742+FssAy@users.noreply.github.com>
2023-10-20 18:47:56 +0200

    Add test for the default charsets

--- src/tests.rs
+++ src/tests.rs
@@ -12,3 +12,33 @@ fn unicode_generation() {
     let result = generate(6, charset).chars().count();
     assert_eq!(result, 6_usize);
 }
+
+#[test]
+fn default_charsets() {
+    const LEN: usize = 10;
+    let mut charset;
+
+    charset = charsets::ALPHANUMERIC;
+    assert_eq!(
+        generate(LEN, charset).chars().count(),
+        LEN,
+    );
+
+    charset = charsets::ALPHA_LOWER;
+    assert_eq!(
+        generate(LEN, charset).chars().count(),
+        LEN,
+    );
+
+    charset = charsets::ALPHA_UPPER;
+    assert_eq!(
+        generate(LEN, charset).chars().count(),
+        LEN,
+    );
+
+    charset = charsets::NUMERIC;
+    assert_eq!(
+        generate(LEN, charset).chars().count(),
+        LEN,
+    );
+}
